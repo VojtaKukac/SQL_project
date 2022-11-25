e05a17eaf5787c97ac7509714a2a54933ee27889
Vojtěch Kukač <115026941+VojtaKukac@users.noreply.github.com>
2022-11-25 21:16:14 +0100

    Update Research_questions.sql
    Add third question

--- Research_questions.sql
+++ Research_questions.sql
@@ -31,3 +31,29 @@ SELECT
 FROM v_select_cz_payllor_growth
 WHERE Growth IS NOT NULL
 GROUP BY Industry_branch;
+
+-- 2.
+
+-- 3.
+
+CREATE OR REPLACE VIEW v_cz_price_difference AS
+SELECT 
+	Year_price,
+	Category,
+	Price,
+	LEAD (Price,1) OVER (PARTITION BY Category ORDER BY Category, Year_price) Next_year_price,
+	ROUND ((((LEAD (Price,1) OVER (PARTITION BY Category  ORDER BY Category, Year_price)) - Price) / Price) * 100,2) Difference
+FROM 
+	t_vojtech_kukac_project_sql_primary_final
+GROUP BY 
+	Category, Year_price;
+			
+SELECT 
+	Category,  
+	ROUND(AVG (Difference),2)
+FROM 
+	v_cz_price_difference
+GROUP BY
+	Category
+ORDER BY 
+	AVG (Difference);
