340e83826b035bd5cfc934d86d575cea602e0ab2
Vojtěch Kukač <115026941+VojtaKukac@users.noreply.github.com>
2022-11-25 21:24:58 +0100

    Update Research_questions.sql
    add question titles

--- Research_questions.sql
+++ Research_questions.sql
@@ -1,4 +1,4 @@
--- 1.
+-- 1. Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
 
 CREATE OR REPLACE VIEW v_select_cz_payllor AS 
 SELECT 
@@ -32,9 +32,9 @@ FROM v_select_cz_payllor_growth
 WHERE Growth IS NOT NULL
 GROUP BY Industry_branch;
 
--- 2.
+-- 2. Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
 
--- 3.
+-- 3. Která kategorie potravin zdražuje nejpomaleji (je u ní nejnižší percentuální meziroční nárůst)?
 
 CREATE OR REPLACE VIEW v_cz_price_difference AS
 SELECT 
@@ -57,3 +57,7 @@ GROUP BY
 	Category
 ORDER BY 
 	AVG (Difference);
+
+-- 4. Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd (větší než 10 %)?
+
+-- 5. Má výška HDP vliv na změny ve mzdách a cenách potravin? Neboli, pokud HDP vzroste výrazněji v jednom roce, projeví se to na cenách potravin či mzdách ve stejném nebo násdujícím roce výraznějším růstem?
